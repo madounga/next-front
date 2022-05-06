461912c74558d10ba8f5ac8d0fce515aee599faf
madounga <maddoly1@gmail.com>
2022-05-06 12:52:32 +0200

    ajout commentaire

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const indexpage = () => {
   return (
     <div>
-      <h1>hello les gens</h1>
+      <h1>hello</h1>
       <ul>
         <li>
           <Link href="/sign-in">
@@ -16,6 +16,29 @@ const indexpage = () => {
           </Link>
         </li>
       </ul>
+      <div className="mx-auto col-md-8">
+        <h3 className="text-danger">
+          <i>Blog comment</i>
+        </h3>
+        <p>
+          onsectetur adipiscing elit. donec ipsum leo, triste je triste non,
+          volutpat id eros. suspendre finibus gravida dolor, sit amet faucibus
+          dui tempus tincidunt. Ethiam venimeux congue diam, un vehicula varius
+          fair sit amet. donec sed pulvinar Libération. Etiam nisl dolor,
+          gravida non dolor nca, dignissim efficitur mi. Integer nisl tortor,
+          toujours à orci at, iaculis molestie ante. Présent la femme enceinte
+          est. Praesent non sapien molestie ex fermentum vestibule. Cras
+          molestie sans libero, et efficitur eros blandit vitae. Vestibulum vel
+          bibendum risus, je viens de sodales. Site temporaire de Nullam amet
+          tellus ac imperdiet. Donec eget tempus mi.
+        </p>
+
+        <textarea
+          className="form-control"
+          placeholder="Ecrivez votre commentaire"
+        ></textarea>
+        <button className="btn btn-primary">Publier</button>
+      </div>
     </div>
   );
 };
